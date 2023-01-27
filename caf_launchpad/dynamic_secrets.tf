5c6c566dabcec0fdf33a237eceb3a450f3a7a570
Jason Dossett <jtdossett@sep.com>
2023-01-27 15:07:31 +0000

    Update paths

--- caf_launchpad/dynamic_secrets.tf
+++ caf_launchpad/dynamic_secrets.tf
@@ -3,7 +3,7 @@ module "dynamic_keyvault_secrets" {
   # source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
   # version = "5.5.5"
 
-  source = "git::https://github.com/sep/terraform-azurerm-caf.git//modules/security/dynamic_keyvault_secrets"
+  source = "git::https://github.com/sep/terraform-azurerm-caf.git//modules/security/dynamic_keyvault_secrets?ref=sep-mods"
 
   for_each = try(var.dynamic_keyvault_secrets, {})
 
--- caf_solution/add-ons/azure_devops_agent/dynamic_secrets.tf
+++ caf_solution/add-ons/azure_devops_agent/dynamic_secrets.tf
@@ -2,7 +2,7 @@
 module "dynamic_keyvault_secrets" {
   #source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
   version = "~>5.3.0"
-  source = "git::https://github.com/sep/terraform-azurerm-caf.git//modules/security/dynamic_keyvault_secrets"
+  source = "git::https://github.com/sep/terraform-azurerm-caf.git//modules/security/dynamic_keyvault_secrets?ref=sep-mods"
 
 
   for_each = try(var.dynamic_keyvault_secrets, {})
