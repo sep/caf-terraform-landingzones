30bc663ebfae6678b2755fa935a2b48a93f2ec93
Jason Dossett <87334399+jtdossett@users.noreply.github.com>
2023-02-16 12:43:08 -0500

    Deep merge fix for webapp

--- caf_solution/local.webapp.tf
+++ caf_solution/local.webapp.tf
@@ -1,13 +1,10 @@
 locals {
-  webapp = merge(
-    var.webapp,
-    {
-      app_service_environments     = var.app_service_environments
-      app_service_plans            = var.app_service_plans
-      app_services                 = var.app_services
-      azurerm_application_insights = var.azurerm_application_insights
-      function_apps                = var.function_apps
-      static_sites                 = var.static_sites
-    }
-  )
+  webapp = {
+    app_service_environments     = merge( try( var.webapp.app_service_environments, {}), var.app_service_environments )
+    app_service_plans            = merge( try( var.webapp.app_service_plans, {}), var.app_service_plans )
+    app_services                 = merge( try( var.webapp.app_services, {}), var.app_services ) 
+    azurerm_application_insights = merge( try( var.webapp.app_service_environments, {}), var.azurerm_application_insights )
+    function_apps                = merge( try( var.webapp.function_apps, {}), var.function_apps )
+    static_sites                 = merge( try( var.webapp.static_sites, {}), var.static_sites )
+  }
 }
